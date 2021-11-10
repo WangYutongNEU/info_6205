b18b1f29983d304a9ea3cd88c3b91d3297a4465c
WangYutongNEU <wang.yutong@northeastern.edu>
2021-11-10 13:09:46 -0500

    A5

--- src/main/java/edu/neu/coe/info6205/sort/par/Main.java
+++ src/main/java/edu/neu/coe/info6205/sort/par/Main.java
@@ -16,14 +16,15 @@ import java.util.concurrent.ForkJoinPool;
  */
 public class Main {
 
+    static ForkJoinPool pool = new ForkJoinPool(8);
     public static void main(String[] args) {
         processArgs(args);
-        System.out.println("Degree of parallelism: " + ForkJoinPool.getCommonPoolParallelism());
+        System.out.println("Degree of parallelism: " + pool.getParallelism());
         Random random = new Random();
-        int[] array = new int[2000000];
+        int[] array = new int[6000000];
         ArrayList<Long> timeList = new ArrayList<>();
-        for (int j = 50; j < 100; j++) {
-            ParSort.cutoff = 10000 * (j + 1);
+        for (double j = 0.1; j < 1; j+= 0.1) {
+            ParSort.cutoff = (int)Math.round(array.length* j);
             // for (int i = 0; i < array.length; i++) array[i] = random.nextInt(10000000);
             long time;
             long startTime = System.currentTimeMillis();
